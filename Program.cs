6660461ffb985bc9059d04e3f559557f6adb5700
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2024-09-16 20:24:07 +0600

    worker service configure

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
 using worker_service_for_smtp;
+using worker_service_for_smtp.Interfaces;
+using worker_service_for_smtp.services;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<ISmtpService, SmtpService>();
+builder.Services.AddHostedService<SmtpWorker>();
 
 var host = builder.Build();
 host.Run();

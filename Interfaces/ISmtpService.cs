9fd1c6bf47b6fccd093e961a46e06dc908b0242d
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2024-09-16 19:23:28 +0600

    interface added

---
+++ Interfaces/ISmtpService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worker_service_for_smtp.Interfaces
+{
+    public interface ISmtpService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+    }
+}

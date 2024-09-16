e1bd4c225e2adc6635765c38b1b6fa1d48b0bb2b
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2024-09-16 19:23:14 +0600

    Send Email method added

---
+++ services/SmtpService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using worker_service_for_smtp.Interfaces;
+using worker_service_for_smtp.Models;
+
+namespace worker_service_for_smtp.services
+{
+    public class SmtpService : ISmtpService
+    {
+        private readonly IConfiguration _configuration;
+        public SmtpService(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var smtpSettings = _configuration.GetSection("SmtpSettings").Get<SmtpSettings>();
+
+            using (var smtpClient = new SmtpClient(smtpSettings.Host, smtpSettings.Port))
+            {
+                smtpClient.Credentials = new NetworkCredential(smtpSettings.Username, smtpSettings.Password);
+                smtpClient.EnableSsl = smtpSettings.EnableSsl;
+
+                var mailMessage = new MailMessage()
+                {
+                    From = new MailAddress(smtpSettings.From),
+                    Body = body,
+                    Subject = subject,
+                    BodyEncoding = Encoding.UTF8,
+                    IsBodyHtml = true
+                };
+
+                mailMessage.To.Add(toEmail);
+
+                try
+                {
+                    await smtpClient.SendMailAsync(mailMessage);
+                    Console.WriteLine("Email sent successfully.");
+                }catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+    }
+}
